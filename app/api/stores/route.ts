f3933221d3279774d3b396b92c69082451927ae8
Mbugua Ngigi <dmbuguangigi@gmail.com>
2023-07-01 00:47:29 +0300

    Creating the "Create Store" API route Installing Axios

---
+++ app/api/stores/route.ts
@@ -0,0 +1,35 @@
+//STORE API
+
+import prismadb from "@/lib/prismadb";
+import { auth } from "@clerk/nextjs";
+import { NextResponse } from "next/server";
+
+export async function POST(req: Request) {
+  try {
+    const { userId } = auth(); //Using Clerk to authenticate the POST route
+    //Gives us access to the currently logged in user's ID that is trying to create a store
+    const body = await req.json();
+
+    const { name } = body;
+
+    if (!userId) {
+      return new NextResponse("Unauthorized", { status: 401 });
+    }
+
+    if (!name) {
+      return new NextResponse("Name is required", { status: 400 });
+    }
+
+    const store = await prismadb.store.create({
+      data: {
+        name,
+        userId,
+      },
+    });
+
+    return NextResponse.json(store);
+  } catch (error) {
+    console.log("[STORES_POST]", error);
+    return new NextResponse("Internal Error", { status: 500 });
+  }
+}
