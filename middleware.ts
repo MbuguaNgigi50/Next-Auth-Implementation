f6d207080dc73db7d26fe1d5fd0ba50d1dfde805
Mbugua Ngigi <dmbuguangigi@gmail.com>
2023-10-21 11:26:59 +0300

    Changes to the Middleware

--- middleware.ts
+++ middleware.ts
@@ -18,12 +18,7 @@ All other Routes not stated here will be protected by the Next-Auth.js Middlewar
 */
 
 export const config = {
-	matcher: ['/dashboard']
-}
-/*export const config = {
 	matcher: [
-		"/((?!|register|api|login|password-reset|about|faqs|features|pricing|privacy-policy|terms-and-conditions|/).*)",
+		"/((?!register|api|login|password-reset|about|faqs|features|pricing|privacy-policy|terms-and-conditions|/).*)"
 	],
 };
-*/
-
