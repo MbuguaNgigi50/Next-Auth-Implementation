f32eee6a0fb3735b9d8b7b372a11f16c0820e76b
Mbugua Ngigi <dmbuguangigi@gmail.com>
2023-10-21 14:08:20 +0300

    Added the Email Verification path to the Middleware

--- middleware.ts
+++ middleware.ts
@@ -7,21 +7,22 @@ The Middleware is going to be applied to the entire application apart from the f
 	3) Login
 	4) API
 	5) Password Reset
-	6) About
-	7) FAQs
-	8) Features
-	9) Pricing
-	10) Privacy Policy
-	11) Terms & Conditions
-	12) _next/static (static files)
-	13) _next/image (image optimization files)
-	14) favicon.ico (favicon file) 
+	6) Email Verification
+	7) About
+	8) FAQs
+	9) Features
+	10) Pricing
+	11) Privacy Policy
+	12) Terms & Conditions
+	13) _next/static (static files)
+	14) _next/image (image optimization files)
+	15) favicon.ico (favicon file) 
 
 All other Routes not stated here will be protected by the Next-Auth.js Middleware
 */
 
 export const config = {
 	matcher: [
-		'/((?!register|login|api|password-reset|about|faqs|features|pricing|privacy-policy|terms-and-conditions|_next/static|_next/image|favicon.ico).*)',
+		'/((?!register|login|api|password-reset|email-verification|about|faqs|features|pricing|privacy-policy|terms-and-conditions|_next/static|_next/image|favicon.ico).*)',
 	],
 };
