87b9e1126e71cadf202b32cca10c9e40aaefa557
Mbugua Ngigi <dmbuguangigi@gmail.com>
2023-10-30 17:34:19 +0300

    Added a Create-Store Route

--- app/(application)/(root)/page.tsx
+++ app/(application)/(root)/page.tsx
@@ -1,23 +1,10 @@
-'use client'
+import { LoginButton } from '@/components/auth';
 
-import { useStoreModal } from "@/hooks/use-store-modal"
-import { useEffect } from "react";
-
-const SetupPage = () => {
-	const onOpen = useStoreModal((state) => state.onOpen);
-	const isOpen = useStoreModal((state) => state.isOpen);
-
-	useEffect(() => {
-		if (!isOpen) {
-			onOpen();
-		}
-	}, [isOpen, onOpen]);
-
+export default function Home() {
 	return (
-		<div className="p-4">
-			Root Page
-		</div>
-	)
+		<main>
+			<div>This is the homepage</div>
+			<LoginButton />
+		</main>
+	);
 }
-
-export default SetupPage
